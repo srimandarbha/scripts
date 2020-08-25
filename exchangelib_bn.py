268f1e04b46210f4eefa91e75d22782fc9291c3f
srimandarbha <srimandarbha@yahoo.in>
2020-08-26 04:42:45 +0530

    Add files via upload

---
+++ exchangelib_bn.py
@@ -0,0 +1,20 @@
+from exchangelib import Credentials, Account, Configuration, DELEGATE
+
+mail_queue_list = []
+
+credentials = Credentials(username='sdarbha@example.com', password='askmeidontknow')
+config = Configuration(server='outlook.office365.com',credentials=credentials)
+account = Account(primary_smtp_address='sdarbha@example.com', autodiscover=False, config = config, access_type=DELEGATE)
+
+# filter to search subject/body or a word contained 
+# https://github.com/ecederstrand/exchangelib/issues/83
+unread_list = account.inbox.filter(is_read=False and "subject in 'BNAUTO:'")
+
+if unread_list.count() > 0:
+    for mail in unread_list:
+        mail_queue_list.append({mail.subject : mail.body})
+        #To mark a email as read
+        mail.is_read=True
+        mail.save()
+
+print(mail_queue_list)
